547e971ab1924cd52c61c31dc78021a19f8aee03
mahlukganteng <57159041+mahlukganteng@users.noreply.github.com>
2020-10-31 11:44:21 +0700

    Update gofood.php

--- gofood.php
+++ gofood.php
@@ -54,7 +54,7 @@ echo color("red","                 Gunakan Format 62\n");
         echo color("white",".");
         sleep(5);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD2107"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"HARIJADIMART"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","Message: ".$message);
